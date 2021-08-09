521a2fe07913881a57860ce1c92dc0cdbbf1c49b
Antek <marmaros987@gmail.com>
2021-08-09 17:11:59 +0200

    add filters function[C

--- src/js/script.js
+++ src/js/script.js
@@ -33,6 +33,11 @@
   const favoriteBooks = [];
   console.log('favoriteBooks', favoriteBooks);
 
+  const filters = [];
+  console.log('filters', filters)
+  const filter = document.querySelector('.filters')
+  console.log('filter', filter)
+
   function initActions(){
     const containerOfBooks = document.querySelector(select.listOf.booksList);
     console.log('containerOfBooks', containerOfBooks);
@@ -43,6 +48,8 @@
       image.addEventListener(/*'dblclick'*/ 'click', function (event){
         event.preventDefault();
         //image.classList.add('favorite');
+        const image = event.target.offsetParent;
+        console.log('image', image)
         const idBook = image.getAttribute('data-id');
         //favoriteBooks.push(idBook);
         console.log('idBook', idBook);
@@ -59,6 +66,38 @@
         console.log('favoriteBooks', favoriteBooks);
       });
     }
+    filter.addEventListener('change', function(event){
+      event.preventDefault();
+      if (event.target.tagName === 'INPUT' && event.target.type === 'checkbox'  && event.target.name==='filter') {
+        if (event.target.checked) {
+          filters.push(event.target.value);
+          console.log('sprawdź co zostało kliknięte:', event.target.value);
+        } else {
+          filters.splice(filters.indexOf(event.target.value));
+          console.log('sprawdź co zostało kliknięte:', event.target.value);
+        }
+        console.log('sprawdż zawartośc tablicy filters', filters)
+      }
+      bookFilter()
+    });
+    function bookFilter() {
+      for (let book of dataSource.books) {
+        let shouldBeHidden = false;
+        for (const filter of filters) {
+          if (!book.details[filter] === true) {
+            shouldBeHidden = true;
+            break;
+          }
+        }
+        const bookCover = document.querySelector('.book__image[data-id="' + book.id + '"]');
+        if (shouldBeHidden) {
+          bookCover.classList.add('hidden');
+        } else {
+          bookCover.classList.remove('hidden');
+        }
+      }
+    }
   }
+
   initActions();
 }
